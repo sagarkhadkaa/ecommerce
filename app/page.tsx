3de7711c6fd9fdc9f4f57eb0789e415162d93616
Sagar Khadka <sagarkhadka2069@gmail.com>
2024-11-17 21:11:40 +0545

    Fix: image route

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,26 @@ import ImageGallery from './components/ImageGallery';
 import Navbar from './components/Navbar';
 import ProductDescription from './components/ProductDescription';
 
+// const images = [
+//   '/images/image4.jpg',
+//   '/images/image3.jpg',
+//   '/images/image2.jpg',
+//   '/images/image1.jpg',
+//   '/images/image5.jpg',
+//   '/images/image6.jpg',
+// ];
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const basePath = isProd ? '/ecommerce' : '';
+
 const images = [
-  '/images/image4.jpg',
-  '/images/image3.jpg',
-  '/images/image2.jpg',
-  '/images/image1.jpg',
-  '/images/image5.jpg',
-  '/images/image6.jpg',
+  `${basePath}/images/image4.jpg`,
+  `${basePath}/images/image3.jpg`,
+  `${basePath}/images/image2.jpg`,
+  `${basePath}/images/image1.jpg`,
+  `${basePath}/images/image5.jpg`,
+  `${basePath}/images/image6.jpg`,
 ];
 
 export default function HomePage() {
