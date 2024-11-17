6471d7b9466b7ff0bc534d5051fdcc9aa19c6bf4
Sagar Khadka <sagarkhadka2069@gmail.com>
2024-11-17 12:50:58 +0545

    Fix: image overflow

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React, { useState } from 'react';
-// import styles from './ImageGallery.module.scss';
 import styles from './ImageGallary.module.scss';
 
 interface ImageGalleryProps {
